d7a9f277f444630da246ec715251551afab803ee
VilleKokkarinen <kokkarinen.ville@gmail.com>
2020-11-04 15:08:55 +0200

    Updates

---
+++ precache-manifest.fd496ad955b4e20ff606e427e91e334a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46bfa63770317979e4d7",
+    "url": "webohjelmointi2.netlify.com/static/js/runtime~main.ff270fc7.js"
+  },
+  {
+    "revision": "46cd909f3af61d4f93fc",
+    "url": "webohjelmointi2.netlify.com/static/js/main.aae451d2.chunk.js"
+  },
+  {
+    "revision": "e0dc8a1e836d33125553",
+    "url": "webohjelmointi2.netlify.com/static/js/2.e15a07ec.chunk.js"
+  },
+  {
+    "revision": "46cd909f3af61d4f93fc",
+    "url": "webohjelmointi2.netlify.com/static/css/main.2938c8d1.chunk.css"
+  },
+  {
+    "revision": "eca35e0c25e7865b9f9d57dd1785f1eb",
+    "url": "webohjelmointi2.netlify.com/index.html"
+  }
+];
